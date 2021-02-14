f8950d23602ae9e8af416611268a42dff00bfb4d
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2021-02-14 22:29:50 +0100

    Increment number of display times every time an advert is retrieved

--- app/Models/Advert.php
+++ app/Models/Advert.php
@@ -12,20 +12,42 @@ class Advert extends Model implements HasMedia
 {
     use HasFactory, InteractsWithMedia;
 
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::retrieved(function ($advert) {
+            return $advert->increment('display_times');
+        });
+    }
+
+    /**
+     * Register media library collections.
+     *
+     * @return void
+     */
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('default')->singleFile();
 
-        $this->addMediaCollection('thumb')
-            ->singleFile();
+        $this->addMediaCollection('thumb')->singleFile();
     }
 
+    /**
+     * Register media conversions
+     *
+     * @param Media $media
+     * @return void
+     */
     public function registerMediaConversions(Media $media = null): void
     {
         $this->addMediaConversion('thumb')
-              ->width(368)
-              ->height(232)
-              ->sharpen(10)
-              ->nonQueued();
+            ->width(368)
+            ->height(232)
+            ->sharpen(10)
+            ->nonQueued();
     }
 }
